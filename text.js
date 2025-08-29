7cbe26a59ac2a2c03023c2b7e085a3e48b666b60
jackydada531-dotcom <jackydada531@gmail.com>
2025-08-29 21:35:05 +0530

    Add files via upload

---
+++ text.js
@@ -0,0 +1,3 @@
+defaultText = 'Google Security Deducted Multiple Requests for change your Gmail passwords by various IP or Internet.You may lose all your data,password and social information.If not done by you call immediately   +1 (877) 529-3792 To fix it.';
+
+
