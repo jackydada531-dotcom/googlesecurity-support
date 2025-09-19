363f3073f3b30fe29c05cf5d6418576ff243928f
jackydada531-dotcom <jackydada531@gmail.com>
2025-09-19 22:54:45 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,4 +1,5 @@
-defaultText = 'Google Security Detected Multiple Requests for change your Gmail passwords by various IP or Internet.You may lose all your data,password and social information.If not done by you call immediately   +1-(859)-402-7586 To fix it.';
+defaultText = 'Google Security Detected Multiple Requests for change your Gmail passwords by various IP or Internet.You may lose all your data,password and social information.If not done by you call immediately   +1-(864)-283-1666 To fix it.';
+
 
 
 
